e77055c6525fd591ef8a58c79bc721dbc24d7bed
steveimc <steveimc08@gmail.com>
2015-09-22 21:06:16 -0700

    KillBox

---
+++ Assets/Scripts/KillBox.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillBox : MonoBehaviour 
+{
+	public Transform spawnPoint;
+
+
+	private void OnTriggerEnter2D(Collider2D col2D)
+	{
+		if(col2D.gameObject.GetComponent<HeroStatus>() != null)
+		{
+			col2D.transform.position = spawnPoint.position;
+		}
+	}
+}
