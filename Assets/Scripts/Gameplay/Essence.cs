9e66b1dd58acac9559664520b11632f7005770ff
steveimc <steveimc08@gmail.com>
2015-10-04 19:02:17 -0700

    Essence Prefab

--- Assets/Scripts/Gameplay/Essence.cs
+++ Assets/Scripts/Gameplay/Essence.cs
@@ -7,6 +7,15 @@ public class Essence : MonoBehaviour
 	TODO: 
 	Add Particle component
 	 */
+	private void OnTriggerEnter2D(Collider2D col2D)
+	{
+		if(col2D.GetComponent<HeroStatus>())
+		{
+			OnCapture();
+			Destroy(this.gameObject);
+		}
+	}
+
 	public void OnCapture()
 	{
 		GameManager.Instance.OnEssenceCaptured();
