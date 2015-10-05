150af02798ce2a1f1ec8536a0fdbf83df4fdc2e6
andreiamfg <andreia.mfg@gmail.com>
2015-10-04 17:55:07 -0700

    Animated Stage Banners

---
+++ Assets/Scripts/User Interface/PlayMovieTexture.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayMovieTexture : MonoBehaviour 
+{
+	public void Start()
+	{
+		MovieTexture movie = (MovieTexture)this.GetComponent<RawImage>().mainTexture;
+		movie.Play();
+	}
+}
---
+++ Assets/Scripts/User Interface/UI Elements/StageBanners.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StageBanners : MonoBehaviour 
+{
+	[SerializeField] private Animator m_BannersAnimator;
+
+	private const string TRIGGER_STAGE_1 = "Stage_1";
+	private const string TRIGGER_STAGE_2 = "Stage_2";
+	private const string TRIGGER_STAGE_3 = "Stage_3";
+	private const string TRIGGER_STAGE_4 = "Stage_4";
+	private const string TRIGGER_STAGE_5 = "Stage_5";
+	private const string TRIGGER_STAGE_FINAL = "Stage_6";
+
+	public void ShowBanner( int stage )
+	{
+		switch(stage)
+		{
+		case 1:
+			m_BannersAnimator.SetTrigger(TRIGGER_STAGE_1);
+			break;
+		case 2:
+			m_BannersAnimator.SetTrigger(TRIGGER_STAGE_2);
+			break;
+		case 3:
+			m_BannersAnimator.SetTrigger(TRIGGER_STAGE_3);
+			break;
+		case 4:
+			m_BannersAnimator.SetTrigger(TRIGGER_STAGE_4);
+			break;
+		case 5:
+			m_BannersAnimator.SetTrigger(TRIGGER_STAGE_5);
+			break;
+		case 6:
+			m_BannersAnimator.SetTrigger(TRIGGER_STAGE_FINAL);
+			break;
+		}
+	}
+}
